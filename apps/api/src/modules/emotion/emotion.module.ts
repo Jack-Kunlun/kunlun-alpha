425484bcc46f0df5f287daac07c573739ac1a97b
风岚 <1837115857@qq.com>
2026-08-13 18:15:53 +0800

    chore: prettier 归一化 emotion.module.ts

--- apps/api/src/modules/emotion/emotion.module.ts
+++ apps/api/src/modules/emotion/emotion.module.ts
@@ -4,10 +4,7 @@ import { EMOTION_SNAPSHOTS, EmotionService } from "./emotion.service";
 
 @Module({
   controllers: [EmotionController],
-  providers: [
-    EmotionService,
-    { provide: EMOTION_SNAPSHOTS, useValue: [] },
-  ],
+  providers: [EmotionService, { provide: EMOTION_SNAPSHOTS, useValue: [] }],
   exports: [EmotionService],
 })
 export class EmotionModule {}
